c681c4ca1693e0a884a669c69c6a553c1999de5d
Dave van Soest <dave.soest@gfk.com>
2014-05-14 11:51:24 +0200

    Meteor package definition.

---
+++ package.js
@@ -0,0 +1,34 @@
+Package.describe({
+    summary: 'CarbonRouter - An alternative, lightweight, modular client side router for Meteor'
+});
+
+
+Package.on_use(function (api) {
+    api.use('underscore', 'client');
+    api.use('templating', 'client');
+    api.use('ui', 'client');
+    api.use('session', 'client');
+
+    api.add_files('lib/util.js', 'client');
+
+    api.add_files('lib/client/layout.html', 'client');
+    api.add_files('lib/client/util.js', 'client');
+    api.add_files('lib/client/router.js', 'client');
+    api.add_files('lib/client/template.js', 'client');
+    api.add_files('lib/client/app.js', 'client');
+
+    api.export('Router', 'client');
+
+    api.export('Util', 'client', {testOnly: true});
+    api.export('CarbonRouter', 'client', {testOnly: true});
+});
+
+
+Package.on_test(function (api) {
+    api.use('carbon-router', ['client', 'server']);
+    api.use('tinytest', ['client', 'server']);
+
+    api.add_files('test/util.js', 'client'); // TODO
+    api.add_files('test/client/router.js', 'client'); // TODO
+});
+
