697eaaf161ad4a44a0d51781b7b0d94af6fe221b
Dave van Soest <dave.soest@gfk.com>
2014-07-03 15:57:31 +0200

    Implemented redesign. Bumped to version 0.1.0!

--- test/client/router.js
+++ test/client/router.js
@@ -9,8 +9,13 @@ Tinytest.add('#CarbonRouter - Set and read configuration', function(test) {
 Tinytest.add('#CarbonRouter - Default configuration values', function(test) {
     var router = new CarbonRouter();
     router.configure({});
-    test.equal(router.getConfig('contentKey'), 'yield', 'Default config value for contentKey is yield.');
     test.equal(router.getConfig('autoLoad'), true, 'Default config value for autoLoad is true.');
+
+    var regionsConfig = router.getConfig('regions');
+    test.equal(regionsConfig.layout.template, 'carbon__default_layout', 'Default config value for layout region template.');
+    test.equal(regionsConfig.layout.data, {}, 'Default config value for layout region data.');
+    test.equal(regionsConfig.content.template, 'carbon__default_content', 'Default config value for content region template.');
+    test.equal(regionsConfig.content.data, {}, 'Default config value for content region data.');
 });
 
 
@@ -67,80 +72,81 @@ Tinytest.add('#CarbonRouter - Go to URL and test current controller', function(t
     var content = null;
 
     router.configure({
-        layoutTemplate: 'tpl_global_layout',
-        loading: {
-            layoutTemplate: 'tpl_loading_layout',
-            contentTemplate: 'tpl_loading_content'
-        },
-        notFound: {
-            layoutTemplate: 'tpl_not_found_layout',
-            contentTemplate: 'tpl_not_found_content'
+        regions: {
+            layout: {
+                template: 'tpl_global_layout',
+            }
         }
     });
 
     router.add('uno', '/uno', {
-        contentTemplate: 'tpl_uno'
+        regions: {
+            content: { template: 'tpl_uno' }
+        }
     });
     router.add('oneParam', '/prefix/{x}', {
-        contentTemplate: 'tpl_oneParam',
-        layoutTemplate: 'tpl_private_layout'
+        regions: {
+            content: { template: 'tpl_oneParam' },
+            layout: { template: 'tpl_private_layout' }
+        }
     });
     router.add('twoParams', '/prefix-{x}/{y}', {
-        contentTemplate: 'tpl_twoParams'
+        regions: {
+            content: { template: 'tpl_twoParams' }
+        }
     });
 
     controller = router.current();
-    layout = controller.getLayoutTemplateAndData();
-    content = controller.getContentTemplateAndData();
-    test.equal(content.template(), Template['tpl_loading_content'], 'Content template for loading from router config.');
-    test.equal(layout.template, Template['tpl_loading_layout'], 'Layout template for loading from router config.');
+    content = controller.getRegionTemplateAndData('content');
+    test.equal(content.data().carbon_status, CarbonController.STATUS.LOADING, 'Carbon status is "' + CarbonController.STATUS.LOADING + '" when there is no current URL.');
 
     router.go('uno');
     controller = router.current();
-    layout = controller.getLayoutTemplateAndData();
-    content = controller.getContentTemplateAndData();
-    test.equal(content.template(), Template['tpl_uno'], 'Content template from route.');
-    test.equal(layout.template, Template['tpl_global_layout'], 'Layout template from router config because not specified in route.');
+    layout = controller.getRegionTemplateAndData('layout');
+    content = controller.getRegionTemplateAndData('content');
+    test.equal(content.template, Template['tpl_uno'], 'Content region template from route.');
+    test.equal(layout.template, Template['tpl_global_layout'], 'Layout region template from router config because not specified in route.');
 
     router.goUrl('/prefix/123');
     controller = router.current();
-    layout = controller.getLayoutTemplateAndData();
-    content = controller.getContentTemplateAndData();
-    test.equal(content.template(), Template['tpl_oneParam'], 'Content template from route.');
-    test.equal(layout.template, Template['tpl_private_layout'], 'Layout template from route.');
-    test.equal(content.data().x, '123', 'URL parameter in content data.');
-    test.equal(content.data().x, '123', 'URL parameter in layout data.');
+    layout = controller.getRegionTemplateAndData('layout');
+    content = controller.getRegionTemplateAndData('content');
+    test.equal(content.template, Template['tpl_oneParam'], 'Content region template from route.');
+    test.equal(layout.template, Template['tpl_private_layout'], 'Layout region template from route.');
+    test.equal(content.data().x, '123', 'URL parameter in content region data.');
+    test.equal(content.data().x, '123', 'URL parameter in layout region data.');
 
     router.go('twoParams', {x: 123, y: 456});
     controller = router.current();
-    layout = controller.getLayoutTemplateAndData();
-    content = controller.getContentTemplateAndData();
-    test.equal(content.template(), Template['tpl_twoParams'], 'Content template from route.');
-    test.equal(layout.template, Template['tpl_global_layout'], 'Layout template from router config because not specified in route.');
-    test.equal(content.data().x, '123', 'URL parameters in content data.');
-    test.equal(content.data().x, '123', 'URL parameters in layout data.');
-    test.equal(content.data().y, '456', 'URL parameters in content data.');
-    test.equal(content.data().y, '456', 'URL parameters in layout data.');
+    layout = controller.getRegionTemplateAndData('layout');
+    content = controller.getRegionTemplateAndData('content');
+    test.equal(content.template, Template['tpl_twoParams'], 'Content region template from route.');
+    test.equal(layout.template, Template['tpl_global_layout'], 'Layout region template from router config because not specified in route.');
+    test.equal(content.data().x, '123', 'URL parameters in content region data.');
+    test.equal(content.data().x, '123', 'URL parameters in layout region data.');
+    test.equal(content.data().y, '456', 'URL parameters in content region data.');
+    test.equal(content.data().y, '456', 'URL parameters in layout region data.');
 
     router.goUrl('/invalid');
     controller = router.current();
-    layout = controller.getLayoutTemplateAndData();
-    content = controller.getContentTemplateAndData();
-    test.equal(content.template(), Template['tpl_not_found_content'], 'Content template for not-found from router config.');
-    test.equal(layout.template, Template['tpl_not_found_layout'], 'Layout template for not-found from router config.');
-
+    layout = controller.getRegionTemplateAndData('layout');
+    content = controller.getRegionTemplateAndData('content');
+    test.equal(content.data().carbon_status, CarbonController.STATUS.NOT_FOUND, 'Content region data carbon_status "' + CarbonController.STATUS.NOT_FOUND + '" for invalid url path.');
+    test.equal(layout.data().carbon_status, CarbonController.STATUS.NOT_FOUND, 'Layout region data carbon_status "' + CarbonController.STATUS.NOT_FOUND + '" for invalid url path.');
 });
 
 
 Tinytest.add('#CarbonRouter - Before hook', function(test) {
     var router = new CarbonRouter();
-    var beforeHookIsRun = false;
+    var beforeHookIsCalled = false;
     var parameterIsPassedToBeforeHook = false;
 
     router.add('testbeforehook', '/{x}', {
-        contentTemplate: 'tpl_test',
+        regions: {
+            content: { template: 'tpl_test' }
+        },
         before: function(params) {
-            beforeHookIsRun = true;
+            beforeHookIsCalled = true;
             parameterIsPassedToBeforeHook = params.x === '123';
         }
     });
@@ -148,8 +154,7 @@ Tinytest.add('#CarbonRouter - Before hook', function(test) {
     router.goUrl('/123');
     controller = router.current();
     controller.callBeforeHook();
-    test.equal(controller.contentTemplate, 'tpl_test', 'Content template from route.');
-    test.isTrue(beforeHookIsRun, 'Before hook is run.');
+    test.isTrue(beforeHookIsCalled, 'Before hook is run.');
     test.isTrue(parameterIsPassedToBeforeHook, 'Parameter is passed to before hook.');
 });
 
