2842168921613cb600884099b9f8a1a0b6ae4d92
Marco de Jongh <marco.dejongh@gfk.com>
2015-01-28 14:07:10 +0100

    Moved carbon-router to gfk maintainer

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
     summary: 'CarbonRouter - An alternative, lightweight, modular client side router for Meteor',
     version: "0.1.4",
-    name: "mrt:carbon-router",
+    name: "gfk:carbon-router",
     git: "https://github.com/gfk-ba/carbon-router.git"
 });
 
 
 Package.onUse(function (api) {
-    api.versionsFrom('0.9.0');
+    api.versionsFrom('1.0');
 
     api.use('underscore', 'client');
     api.use('templating', 'client');
@@ -32,11 +32,10 @@ Package.onUse(function (api) {
 
 
 Package.onTest(function (api) {
-    api.use('mrt:carbon-router', ['client', 'server']);
+    api.use('gfk:carbon-router', ['client', 'server']);
     api.use('tinytest', ['client', 'server']);
 
     api.addFiles('test/util.js', 'client');
     api.addFiles('test/client/controller.js', 'client');
     api.addFiles('test/client/router.js', 'client');
 });
-
